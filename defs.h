acad0b3e78327d41a68a55b6a37dafa393f398ee
Anatol Belski <ab@php.net>
2014-10-01 10:22:42 +0200

    added defs.h

---
+++ defs.h
@@ -0,0 +1,32 @@
+
+#ifdef _WIN32
+
+# define _TLS __declspec(thread)
+
+# ifdef IMPLICIT_EXPORTS
+#  define IMPLICIT_API __declspec(dllexport)
+# else
+#  define IMPLICIT_API __declspec(dllimport)
+# endif
+
+# ifdef EXPLICIT_EXPORTS
+#  define EXPLICIT_API __declspec(dllexport)
+# else
+#  define EXPLICIT_API __declspec(dllimport)
+# endif
+
+#else
+
+# define _TLS __thread
+
+#  if defined(__GNUC__) && __GNUC__ >= 4
+#   define IMPLICIT_API __attribute__ ((visibility("default")))
+#   define EXPLICIT_API __attribute__ ((visibility("default")))
+#  else
+#   define IMPLICIT_API
+#   define EXPLICIT_API
+#  endif
+
+#endif
+
+
